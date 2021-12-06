110a6a15b567ea17b9d94e066ec1472910cf16ec
Alexey Shapovalov <alexei.shapovalov.42@gmail.com>
2021-12-07 01:23:22 +0200

    Added exr change page API

--- app/src/main/java/com/example/magdadmilbat/ExrChoiceScreen.java
+++ app/src/main/java/com/example/magdadmilbat/ExrChoiceScreen.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.magdadmilbat.database.settings.SettingsManager;
+
 public class ExrChoiceScreen extends AppCompatActivity {
 
     @Override
@@ -11,4 +13,14 @@ public class ExrChoiceScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exr_choice_screen);
     }
+
+    /**
+     * Load a setting profile and go to the exercise screen
+     * @param profileToLoad
+     */
+    private void goToExrScreen(String profileToLoad){
+        SettingsManager manager = SettingsManager.getInstance();
+
+    }
+
 }
