4478d6a746c0c434613cc10045f4d8521ae4eacb
Yonatanp2005 <95964905+Yonatanp2005@users.noreply.github.com>
2021-12-16 14:16:02 +0200

    Update ExercisePage.java

--- app/src/main/java/com/example/magdadmilbat/ExercisePage.java
+++ app/src/main/java/com/example/magdadmilbat/ExercisePage.java
@@ -6,9 +6,23 @@ import android.os.Bundle;
 
 public class ExercisePage extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    companion bobject {
+        private const val camara_permission_code = 1;
+        private const val camara = 2;
+    }
+    
+    
+    override fun onCreate(savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_page);
+        
+        btn_camera.setOnClickListener {it:View!
+            if(ContextCompat.checkselfpermission(context:this,Manifast.permission.camera)
+               == PackageManager.PERMISSION_GRANTED)
+            {
+             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            
     }
+    
+    
 }
