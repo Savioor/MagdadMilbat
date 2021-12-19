070cc66b4e6e3e7f4839f3a34fa7248797c1d121
yoavarviv <yoavarviv1@gmail.com>
2021-12-19 18:10:57 +0200

    working with first and second video, displaying percentage of frames the ball has been up, check documentation for more info.

--- app/src/main/java/com/example/magdadmilbat/main.java
+++ app/src/main/java/com/example/magdadmilbat/main.java
@@ -11,12 +11,15 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 public class main {
+    private static final String filename = "src/firstvid.mp4";
     public static void main(String[] args){
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // Loading the OpenCV library.
+        int numOfFrames = 0;
         // Mat img = loadImage("src/Screenshot_2.png"); // Getting the image.
-        VideoCapture vid = new VideoCapture("src/firstvid.mp4");
+        VideoCapture vid = new VideoCapture(filename);
         Mat img = new Mat();
         JFrame jframe = new JFrame("Video"); // We create a new JFrame object.
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // We inform jframe what to do when we close the program.
@@ -24,16 +27,25 @@ public class main {
         jframe.setContentPane(vidPanel); // We assign vidpanel to the jframe we created.
         jframe.setSize(2000, 4000); // We set the frame size to 2000x4000
         jframe.setVisible(true); // We make the jframe visible.
+        Mat procImg;
+        Mat circles;
+        int initialY = 0;
+        int firstBallFramesCounter = 0, secondBallFramesCounter = 0, thirdBallFramesCounter = 0, countOfBalls;
 
         while(vid.read(img)) {
-            Mat procImg = prepareImage(img); // We prepare the image.
+            if(numOfFrames == 0){
+                initialY = getFrameData(img);
+            }
+            procImg = prepareImage(img); // We prepare the image.
 
-        /*
+            /*
             We apply the HoughCircles algorithm, and get an array named circles that represents each circle we find.
             https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga47849c3be0d0406ad3ca45db65a25d2d
-         */
-            Mat circles = new Mat(); // Will represent the circles we find.
-            Imgproc.HoughCircles(procImg, circles, Imgproc.HOUGH_GRADIENT, 1.0, 80, 105.0, 28.0, 40, 100);
+            */
+            circles = new Mat(); // Will represent the circles we find.
+            Imgproc.HoughCircles(procImg, circles, Imgproc.CV_HOUGH_GRADIENT, 1.0, 80, 110.0, 28.0, 40, 100); // param2 = 25 for vid2, param2 = 28 for vid1
+
+            countOfBalls = 0;
             for (int x = 0; x < circles.cols(); x++) {
                 double[] c = circles.get(0, x); // Getting the circle, c is in the format of: {x, y, radius}.
                 Point center = new Point(Math.round(c[0]), Math.round(c[1]));
@@ -42,13 +54,43 @@ public class main {
                 // Drawing the circle's outlines.
                 int radius = (int) Math.round(c[2]);
                 Imgproc.circle(img, center, radius, new Scalar(255, 0, 255), 3, 8, 0);
+                if(center.y >= initialY - 30 && center.y <= initialY + 30) countOfBalls++; // Only counts the balls that are in the range of the initial Y axis position.
             }
+            /**
+             *
+             */
+            switch(countOfBalls){
+                case 3:
+                    firstBallFramesCounter++;
+                case 2:
+                    secondBallFramesCounter++;
+                case 1:
+                    thirdBallFramesCounter++;
+                    break;
+            }
+            System.out.println(countOfBalls); // Debugging print.
             ImageIcon result = new ImageIcon(Mat2BufferedImage(img));
             vidPanel.setIcon(result);
             vidPanel.repaint();
-
-            // showImage(img);
+            numOfFrames++;
        }
+
+        System.out.println("Final Analysis: " + "\nFirst Ball: " + firstBallFramesCounter * 100 /numOfFrames
+                + "%\nSecond Ball: " + secondBallFramesCounter * 100 / numOfFrames
+                + "%\nThird Ball: " + thirdBallFramesCounter * 100 / numOfFrames + "%");
+    }
+
+    /**
+     * This function takes the first frame and gets the initial position of the balls. It returns the initial Y axis position.
+     * @param img the frame we analyze.
+     * @return the initial Y axis position.
+     */
+    private static int getFrameData(Mat img) {
+        Mat procImg = prepareImage(img);
+        Mat circles = new Mat();
+
+        Imgproc.HoughCircles(procImg, circles, Imgproc.CV_HOUGH_GRADIENT, 1.0, 80, 95.0, 26.0, 40, 100);
+        return (int)Math.round(circles.get(0, 0)[1]);
     }
 
     /**
@@ -78,12 +120,15 @@ public class main {
     public static Mat prepareImage(Mat img){
         Mat dest = new Mat(); // Creating a destination Matrix (Image) for the grayscale.
         Imgproc.cvtColor(img, dest, Imgproc.COLOR_RGB2GRAY); // We grayscale the image to get it to binary form
-        Imgproc.medianBlur(dest, dest, 3); // We blur the grayscale image using a kernel size of 7.
+        Imgproc.medianBlur(dest, dest, 3); // We blur the grayscale image using a kernel size of 3.
         return dest;
     }
 
+    /**
+     * This function converts a matrix to a buffered image.
+     * @param m the matrix we convert.
+     */
     public static BufferedImage Mat2BufferedImage(Mat m) {
-        //Method converts a Mat to a Buffered Image
         int type = BufferedImage.TYPE_BYTE_GRAY;
         if ( m.channels() > 1 ) {
             type = BufferedImage.TYPE_3BYTE_BGR;
