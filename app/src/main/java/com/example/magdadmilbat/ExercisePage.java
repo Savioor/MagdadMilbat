b7a7385bee0b2d051954000305dbe73d000c8ee9
yaelblum <yael.blum.42@gmail.com>
2021-12-21 16:53:47 +0200

    finish feedback

--- app/src/main/java/com/example/magdadmilbat/ExercisePage.java
+++ app/src/main/java/com/example/magdadmilbat/ExercisePage.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//import com.example.MagdadMilbat.FeedbackPage;
+//import com.example.magdadmilbat.FeedbackPage;
 import com.example.MagdadMilbat.R;
 
 import java.time.LocalDateTime;
---
+++ app/src/main/java/com/example/magdadmilbat/FeedbackPage.java
@@ -0,0 +1,33 @@
+package com.example.magdadmilbat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.MagdadMilbat.R;
+import com.example.magdadmilbat.ExercisePage;
+import com.example.magdadmilbat.MainActivity;
+
+public class FeedbackPage extends AppCompatActivity implements View.OnClickListener {
+    Button btnBackMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback_page);
+
+        btnBackMain = (Button) findViewById(R.id.btnSettings);
+
+        btnBackMain.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        if (view == btnBackMain) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
