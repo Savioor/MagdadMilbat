7708df9d7ffd8bdfa863900a485b3a03d7f34249
yaelblum <yael.blum.42@gmail.com>
2021-12-22 15:45:09 +0200

    סידור

--- app/src/main/java/com/example/magdadmilbat/ExercisePage.java
+++ app/src/main/java/com/example/magdadmilbat/ExercisePage.java
@@ -2,6 +2,7 @@ package com.example.magdadmilbat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
