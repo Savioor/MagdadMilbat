1492fdcd4c1d543d11cbac1e895db8e41897ab94
HDChaiS <hadch@post.bgu.ac.il>
2021-12-22 04:03:25 +0200

    Add trainig database functional

--- app/src/main/java/com/example/magdadmilbat/HistoryPage.java
+++ app/src/main/java/com/example/magdadmilbat/HistoryPage.java
@@ -3,12 +3,15 @@ package com.example.magdadmilbat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.example.magdadmilbat.database.DatabaseManager;
 
 import java.util.ArrayList;
 
@@ -19,11 +22,17 @@ public class HistoryPage extends AppCompatActivity implements AdapterView.OnItem
     private ArrayList<Training> trainings;
     private String[] trainingHistory;
     private String curr;
+    private DatabaseManager databaseManager;
+    private SQLiteDatabase sqLiteDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_page);
+        databaseManager = new DatabaseManager(this);
+        sqLiteDatabase = databaseManager.getWritableDatabase();
+        sqLiteDatabase.close();
+        databaseManager.addTraining(new Training("18/1/2002", "2:00", "Breathe", 8));
         lv = (ListView) findViewById(R.id.lvHistory);
         loadHistory();
         curr = "None";
@@ -34,7 +43,7 @@ public class HistoryPage extends AppCompatActivity implements AdapterView.OnItem
      * populate the history page
      */
     private void loadHistory() {
-        trainings = new ArrayList<>();//TODO: Get data  from Database
+        trainings = databaseManager.getAllTraining();
         arraylistToStringArray();
         adap = new ArrayAdapter(this, android.R.layout.simple_list_item_1, trainingHistory);
         lv.setAdapter(adap);
