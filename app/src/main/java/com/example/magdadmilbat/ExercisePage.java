1bd35e941141f01b252a6bd6dd152768a7a84324
neti96 <neta.rose9615@gmail.com>
2021-12-23 23:22:46 +0200

    camera add

--- app/src/main/java/com/example/magdadmilbat/ExercisePage.java
+++ app/src/main/java/com/example/magdadmilbat/ExercisePage.java
@@ -1,10 +1,12 @@
 package com.example.magdadmilbat;
 
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +20,9 @@ import com.example.MagdadMilbat.R;
 public class ExercisePage extends AppCompatActivity implements View.OnClickListener {
     Button btnBack, btnFeedback;
     TextView tvRepetition, tvExercise;
+    Camera camera;
+    FrameLayout cameraFrameLayout;
+    ShowCamera showCamera;
 
     /**
      * on create func - contains return button
@@ -37,6 +42,11 @@ public class ExercisePage extends AppCompatActivity implements View.OnClickListe
         tvRepetition = (TextView) findViewById(R.id.tvRepetition);
         tvExercise = (TextView) findViewById(R.id.tvExercise);
 
+        // open camera
+        cameraFrameLayout = findViewById(R.id.cameraFrameLayout);
+        camera = Camera.open();
+        showCamera = new ShowCamera(this, camera);
+        cameraFrameLayout.addView(showCamera);
     }
     /**
      * returns to the main page when click on the back button
--- app/src/main/java/com/example/magdadmilbat/ShowCamera.java
+++ app/src/main/java/com/example/magdadmilbat/ShowCamera.java
@@ -1,2 +1,62 @@
-package com.example.magdadmilbat;public class ShowCamera {
+package com.example.magdadmilbat;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+
+public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback
+{
+    Camera camera;
+    SurfaceHolder holder;
+
+    public ShowCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+
+    }
+
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        Camera.Parameters params = camera.getParameters();
+
+        if(this.getResources().getConfiguration().orientation!= Configuration.ORIENTATION_LANDSCAPE) {
+            params.set("Orientation","portrait");
+            camera.setDisplayOrientation(90);
+            params.setRotation(90);
+        }
+        else {
+            params.set("orientation","landscape");
+            camera.setDisplayOrientation(0);
+            params.setRotation(0);
+        }
+
+        camera.setParameters(params);
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
+
